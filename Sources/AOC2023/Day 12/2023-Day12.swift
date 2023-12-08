eb1c21fdd9b4137fc4a95fd55750d6660dc7c8d5
yuyuUI <yu.yu@ui.com>
2023-12-08 17:04:29 +0800

    2023

---
+++ Sources/AOC2023/Day 12/2023-Day12.swift
@@ -0,0 +1,27 @@
+//
+//  Day12.swift
+//  AOC2023
+//
+//  Created by Dave DeLong on 12/8/23.
+//  Copyright © 2023 Dave DeLong. All rights reserved.
+//
+
+class Day12: Day {
+    
+    static var rawInput: String? { nil }
+
+    func part1() async throws -> String {
+        return #function
+    }
+
+    func part2() async throws -> String {
+        return #function
+    }
+
+    func run() async throws -> (String, String) {
+        let p1 = try await part1()
+        let p2 = try await part2()
+        return (p1, p2)
+    }
+
+}
